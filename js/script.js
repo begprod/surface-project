40ffc06db77c5a55485526c69c1f28491b1970cf
Andrey Aratov <madingame@yandex.ru>
2017-06-18 17:02:38 +0300

    2nd & 3rd sections

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
-$(document).ready(function () {
-	work();
-	parallaxEff();
-});
+$(window).on('scroll', function() {
 
+	var wScroll = $(this).scrollTop();
 
-function work() {
-	console.log('js work');
-}
-
-function parallaxEff() {
-	var parallaxEl = $('.maintext__prlx-el');
-	$(window).on('scroll', function() {
-		var wScroll = $(this).scrollTop();
-		parallaxEl.css({
-			'top': wScroll/13 + 'px'
-		});
-		$('.maintext').css({
-			'margin-top': '-' + wScroll/2 + 'px'
-		});
+	var parallaxLine = $('.maintext__prlx-line');
+	var parallaxTextEl = $('.maintext');
+	parallaxLine.css({
+		'top': wScroll/6 + 'px'
 	});
-	//console.log(parallaxElTop);
-}
+	parallaxTextEl.css({
+		'margin-top': '-' + wScroll/2 + 'px'
+	});
+
+	var img1 = $('.prlx-footballer').offset().top - 400;
+	$('.prlx-footballer').css({
+		'top': wScroll/20 + 'px',
+		'left': wScroll/5 + 'px'
+	});
+	$('.prlx-footballer .prlx-footballer__prlx-line').css({
+		'top': wScroll/20 + 'px'
+	});
+
+	var img1 = $('.prlx-basketballer').offset().top - 400;
+	$('.prlx-basketballer').css({
+		'top': wScroll/20 + 'px',
+		'right': wScroll/18 + 'px'
+	});
+	$('.prlx-basketballer .prlx-basketballer__prlx-line1').css({
+		'top': wScroll/20 + 'px'
+	});
+	$('.prlx-basketballer .prlx-basketballer__prlx-line2').css({
+		'bottom': wScroll/20 + 'px'
+	});
+});
